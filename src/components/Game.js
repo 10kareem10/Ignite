e4983a3f2dd1d6f29c65a1f09bc99b786857f54b
10kareem10 <karimelihab6@gmail.com>
2022-02-20 23:21:28 +0200

    7.toggeling components

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,6 +12,7 @@ const Game =({name,released,image,id})=>{
 
   const dispatch=useDispatch()
   const loadDetailHandler=()=>{
+    document.body.style.overflow='hidden'; 
     dispatch(loadDetail(id));
 
   }
