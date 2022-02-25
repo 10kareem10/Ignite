25974e246b35c211a7ddf933b059aee97792f83e
10kareem10 <karimelihab6@gmail.com>
2022-02-25 23:07:25 +0200

    11.adding nav and logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import GlobalStyles from "./components/GlobalStyles";
 import GameDetail from "./components/GameDetail";
 import { Route } from "react-router-dom";
+import Nav from "./components/Nav";
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      {/* <Nav /> */}
+      <Nav/>
       <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
